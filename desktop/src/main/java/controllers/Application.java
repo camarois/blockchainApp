d784b8d5fcbc15674e7f69413c401a7554d52ddf
Charles-Auguste Marois <charles.marois.quebec@gmail.com>
2019-09-05 12:41:50 -0400

    Add test http request

--- desktop/src/main/java/controllers/Application.java
+++ desktop/src/main/java/controllers/Application.java
@@ -1,9 +1,31 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import java.net.*;
+import java.io.*;
 
 public class Application {
-    public void onClickHelloWorld(ActionEvent event) {
-        System.out.println("Hello World!");
+    public void onClickHelloWorld(ActionEvent event) throws Exception {
+
+        URL url = new URL("http://localhost:10000/lol");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/atom+xml");
+        int status = conn.getResponseCode();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        System.out.println(status);
+        System.out.println(content);
+
+        conn.disconnect();
     }
 }
+
