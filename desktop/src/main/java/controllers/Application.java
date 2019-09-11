63625e284c472f4692a5f3aff776944915048ee2
Charles-Auguste Marois <charles.marois.quebec@gmail.com>
2019-09-11 17:29:45 -0400

    Add xml parser for resource files.

--- desktop/src/main/java/controllers/Application.java
+++ desktop/src/main/java/controllers/Application.java
@@ -1,6 +1,12 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.InputStream;
 import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -8,9 +14,16 @@ import java.net.http.HttpResponse;
 
 public class Application {
     public void onClickHelloWorld(ActionEvent event) throws Exception {
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("values/strings.xml" );
+
+        // Parse the XML file
+        Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(stream);
+        String host = document.getElementsByTagName("host").item(0).getTextContent();
+
+        // Request a response from the provided URL.
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:10000/lol"))
+                .uri(URI.create(host + "/lol"))
                 .build();
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
