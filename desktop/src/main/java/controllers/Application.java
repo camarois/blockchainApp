a749bc283ef371c5e6672e42976b28ec49f9eb86
camarois <charles.marois.quebec@gmail.com>
2019-09-14 12:55:37 -0400

    Add try-catch block and modifiy http response.

--- desktop/src/main/java/controllers/Application.java
+++ desktop/src/main/java/controllers/Application.java
@@ -6,7 +6,9 @@ import org.w3c.dom.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -16,19 +18,20 @@ public class Application {
     public void onClickHelloWorld(ActionEvent event) throws Exception {
         InputStream stream = getClass().getClassLoader().getResourceAsStream("values/strings.xml" );
 
-        // Parse the XML file
         Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(stream);
         String host = document.getElementsByTagName("host").item(0).getTextContent();
 
-        // Request a response from the provided URL.
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(host + "/lol"))
-                .build();
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join();
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(host + "/lol"))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (Exception e) {
+            System.out.println("Exception occurred, http call didn't work");
+        }
     }
 }
 
