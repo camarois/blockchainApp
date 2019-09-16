460e534cffefe6bdf66e793015c62e5d8993f87a
Francis Granger <franki_le_pro@hotmail.com>
2019-09-15 22:19:30 -0400

    test

--- server/clang.sh
+++ server/clang.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -o xtrace
 
-clang-tidy src/*.cc -p build/src/
-# Exit if there are some c++ warnings
-if [ $? -ne 0 ]; then 
-    exit 1
-fi
-clang-format -style=file src/*.cc -output-replacements-xml | grep -c "<replacement " > /dev/null
-# Exit if grep did find replacements.
-if [ $? -ne 1 ]; then 
-    exit 1
-fi
+case "$1" in
+tidy)
+    clang-tidy src/*.cc -p build/src/
+    # Exit if there are some c++ warnings
+    if [ $? -ne 0 ]; then 
+        exit 1
+    fi
+    ;;
+format)
+    clang-format -style=file src/*.cc -output-replacements-xml | grep -c "<replacement " > /dev/null
+    # Exit if grep did find replacements.
+    if [ $? -ne 1 ]; then 
+        exit 1
+    fi
+    ;;
+*)
+    ./clang.sh tidy
+    ./clang.sh format
+    ;;
+esac
