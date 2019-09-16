ef34c82bb4f4bd509f4a47740e8a9c1bd0e8ac63
Francis Granger <franki_le_pro@hotmail.com>
2019-09-15 23:09:09 -0400

    test

--- server/clang.sh
+++ server/clang.sh
@@ -3,14 +3,14 @@ set -o xtrace
 
 case "$1" in
 tidy)
-    clang-tidy src/*.cc -p build/src/
+    clang-tidy src/*.cc -p build/src/ --quiet
     # Exit if there are some c++ warnings
     if [ $? -ne 0 ]; then 
         exit 1
     fi
     ;;
 format)
-    clang-format$2 -style=file src/*.cc -output-replacements-xml | grep -c "<replacement " > /dev/null
+    clang-format$2 -style=file src/*.cc -output-replacements-xml | grep "<replacement "
     # Exit if grep did find replacements.
     if [ $? -ne 1 ]; then 
         exit 1
