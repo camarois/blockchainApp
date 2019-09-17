c50d2e62ac5e2449f5795db4fc2abf5545520cc1
camarois <charles.marois.quebec@gmail.com>
2019-09-17 06:32:56 -0400

    change xlm to json with gson lib.

--- desktop/src/main/java/controllers/Application.java
+++ desktop/src/main/java/controllers/Application.java
@@ -1,30 +1,24 @@
 package controllers;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import javafx.event.ActionEvent;
-import org.w3c.dom.Document;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.net.*;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Application {
     public void onClickHelloWorld(ActionEvent event) throws Exception {
-        InputStream stream = getClass().getClassLoader().getResourceAsStream("values/strings.xml" );
-
-        Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(stream);
-        String host = document.getElementsByTagName("host").item(0).getTextContent();
+        InputStreamReader reader = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("values/strings.json"));
+        JsonObject urls  = new Gson().fromJson(reader, JsonObject.class);
 
         try {
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(host + "/lol"))
+                    .uri(URI.create(urls.get("host").getAsString() + "/lol"))
                     .build();
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -34,4 +28,3 @@ public class Application {
         }
     }
 }
-
