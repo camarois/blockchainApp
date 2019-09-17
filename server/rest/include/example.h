bf301c61f6567e94d2dd6b33694593c642db6bff
Francis Granger <franki_le_pro@hotmail.com>
2019-09-17 19:06:25 -0400

    add include to executable

--- server/rest/include/example.h
+++ server/rest/include/example.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <pistache/http.h>
 #include <pistache/router.h>
 #include <pistache/endpoint.h>
@@ -9,45 +11,18 @@ using namespace std;
 using namespace Pistache;
 using json = nlohmann::json;
 
-namespace Generic {
-
-void handleReady(const Rest::Request&, Http::ResponseWriter response) {
-    static int lol = 0;
-    Example example;
-    json j = {
-        {"lol", lol++},
-        {"example", example.example()}
-    };
-    response.send(Http::Code::Ok, j.dump(4));
-}
-
-}
-
 class StatsEndpoint {
 public:
     StatsEndpoint(Address addr)
         : httpEndpoint(std::make_shared<Http::Endpoint>(addr))
     { }
 
-    void init(size_t thr = 2) {
-        auto opts = Http::Endpoint::options()
-            .threads(thr);
-        httpEndpoint->init(opts);
-        setupRoutes();
-    }
-
-    void start() {
-        httpEndpoint->setHandler(router.handler());
-        httpEndpoint->serve();
-    }
+    void init(size_t thr);
+    void start();
 
 private:
-    void setupRoutes() {
-        using namespace Rest;
-
-        Routes::Get(router, "/lol", Routes::bind(&Generic::handleReady));
-
-    }
+    void setupRoutes();
+    static void handleReady(const Rest::Request&, Http::ResponseWriter response);
 
     std::shared_ptr<Http::Endpoint> httpEndpoint;
     Rest::Router router;
