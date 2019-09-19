cc9263923b2f3a07dbeb3cff2fca7dc3e71371d8
Francis Granger <franki_le_pro@hotmail.com>
2019-09-18 23:51:19 -0400

    add tests to project

---
+++ server/tests/example_test.cc
@@ -0,0 +1,19 @@
+#include "common/example.h"
+#include <gtest/gtest.h>
+
+class ExampleTest : public ::testing::Test {
+protected:
+  void SetUp() override { example = std::make_unique<Example>(); }
+
+  void TearDown() override { example.reset(); }
+
+  std::unique_ptr<Example> example;
+};
+
+TEST_F(ExampleTest, sample_method_1) {
+  ASSERT_EQ(example->example(), 0);
+}
+
+TEST_F(ExampleTest, sample_method_2) {
+  ASSERT_NE(example->example(), 1);
+}
