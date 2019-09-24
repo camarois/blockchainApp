fd8664bd1cc96b094ce975f6b0da7a9b896507b1
Francis Granger <franki_le_pro@hotmail.com>
2019-09-24 13:56:13 -0400

    force clang 8

--- server/lint.sh
+++ server/lint.sh
@@ -3,14 +3,14 @@ set -o xtrace
 
 case "$1" in
 tidy)
-    clang-tidy src/*.cc -p build/src/ --quiet
+    clang-tidy-8 src/*.cc -p build/src/ --quiet
     # Exit if there are some c++ warnings
     if [ $? -ne 0 ]; then 
         exit 1
     fi
     ;;
 format)
-    clang-format$2 -style=file src/*.cc -output-replacements-xml | grep "<replacement "
+    clang-format-8 -style=file src/*.cc -output-replacements-xml | grep "<replacement "
     # Exit if grep did find replacements.
     if [ $? -ne 1 ]; then 
         exit 1
