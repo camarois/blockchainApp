4c76eaf1b5b7c02aad1b801a8132bc8d20baeb2f
Francis Granger <franki_le_pro@hotmail.com>
2019-09-24 21:33:14 -0400

    add fix to linter

--- server/lint.sh
+++ server/lint.sh
@@ -4,16 +4,23 @@ set -o xtrace
 
 case "$1" in
 tidy)
+    if [ ! -z "$2" ]; then
+        fix="-fix"
+    fi
     find -name "*.cpp" -not -path "./tests/*" -not -path "./third_party/*" -not -path "./build/*" | 
-    xargs run-clang-tidy-8 -p build/ -quiet
+    xargs run-clang-tidy-8 -p build/ -quiet $fix
     # Exit if there are some c++ warnings
-    if [ $? -ne 0 ]; then 
+    if [ $? -ne 0 ]; then
         exit 1
     fi
     ;;
 format)
     find -name "*.cpp" -not -path "./third_party/*" -not -path "./build/*" | 
-    xargs clang-format-8 -style=file -output-replacements-xml | grep "<replacement "
+    if [ ! -z "$2" ]; then
+        xargs clang-format-8 -style=file -i
+    else
+        xargs clang-format-8 -style=file -output-replacements-xml | grep "<replacement "
+    fi
     # Exit if grep did find replacements.
     if [ $? -ne 1 ]; then 
         exit 1
