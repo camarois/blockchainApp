cabdc88ce2201ee67842e848bb4fca5e7227e733
Francis Granger <franki_le_pro@hotmail.com>
2019-09-26 16:14:07 -0400

    make it work with android missing user popup

--- android/app/src/main/java/com/example/androidapp/MainActivity.kt
+++ android/app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -6,9 +6,17 @@ import android.widget.Button
 import android.widget.TextView
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONObject
+import com.android.volley.toolbox.RequestFuture
+import android.support.v4.app.SupportActivity
+import android.support.v4.app.SupportActivity.ExtraData
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,24 +31,28 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.lolVar)
         // Instantiate the RequestQueue
         val queue = Volley.newRequestQueue(this)
-        val url = getString(R.string.host) + "/lol"
-
-        // Request a string response from the provided URL.
+        val baseUrl = "https://us-central1-projet3-46f1b.cloudfunctions.net/getServerURL?user=server"
         val stringRequest = StringRequest(
-            Request.Method.GET, url,
-            Response.Listener<String> { response ->
-                // Display the response string.
-                textView.text = response
-
-                try {
-                    val obj = JSONObject(response)
-                    textView.text = obj.getInt("lol").toString()
-                } catch (t: Throwable) {
-                    textView.text = "Malformed response"
-                }
-            },
-            Response.ErrorListener { textView.text = "That didn't work!" })
-        // Add the request to the RequestQueue.
+            Request.Method.GET, baseUrl, { serverUrl ->
+                println(serverUrl)
+                var url = "http://$serverUrl:10000/lol"
+                val jsonObjectRequest = JsonObjectRequest(
+                    Request.Method.GET, url, null, { lol ->
+                        println(lol)
+                        textView.text = lol.toString()
+                        try {
+                            // TODO Gson https://developer.android.com/training/volley/request-custom.html
+                            textView.text = lol.getInt("lol").toString()
+                        } catch (t: Throwable) {
+                            textView.text = "Malformed response"
+                        }
+                    }, {
+                        textView.text = "That didn't work! Lol error"
+                    })
+                queue.add(jsonObjectRequest)
+            }, {
+                textView.text = "That didn't work! Firebase error"
+            })
         queue.add(stringRequest)
     }
 }
