59e5af009feed12915f3088a6920cb23cf491c94
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-09-26 16:18:58 -0400

    server/miner: add address/port CLI parameters

--- server/miner/miner.cpp
+++ server/miner/miner.cpp
@@ -1,14 +1,29 @@
 #include "common/message_helper.hpp"
+#include <gflags/gflags.h>
 #include <iostream>
 #include <string>
 #include <unistd.h>
 #include <zmq.hpp>
 
-int main() {
+DEFINE_string(addr, "127.0.0.1", "REST service address"); // NOLINT
+DEFINE_int32(port, 8000, "REST service port"); // NOLINT
+
+int main(int argc, char* argv[]) {
+  for(int i = 0; i < argc; i++) {
+    if(strcmp("--help", argv[i]) == 0) {
+      argv[i] = "--helpshort";
+    }
+  }
+
+  GFLAGS_NAMESPACE::SetUsageMessage("Blockchain miner service"); // NOLINT
+  GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, false); // NOLINT
+
+  std::string addr = "tcp://" + std::string(FLAGS_addr) + ":" + std::to_string(FLAGS_port);
+
   try {
     zmq::context_t context(1);
     zmq::socket_t socket(context, ZMQ_REQ);
-    socket.connect("tcp://localhost:5555");
+    socket.connect(addr);
 
     while (true) {
       std::cout << "Sending " << std::flush;
