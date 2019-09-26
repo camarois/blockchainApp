8080cee7dd1a4589e7a085c0701732ba937f0aa1
Francis Granger <franki_le_pro@hotmail.com>
2019-09-26 19:43:58 -0400

    finish per user server url

--- android/app/src/main/java/com/example/androidapp/MainActivity.kt
+++ android/app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -1,58 +1,77 @@
 package com.example.androidapp
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import org.json.JSONObject
-import com.android.volley.toolbox.RequestFuture
-import android.support.v4.app.SupportActivity
-import android.support.v4.app.SupportActivity.ExtraData
-import android.support.v4.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import java.util.concurrent.TimeUnit
-
 
 class MainActivity : AppCompatActivity() {
 
+    var serverUrl: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setServerUrlWithDialog()
         setContentView(R.layout.activity_main)
         findViewById<TextView>(R.id.lolVar).text = "lol!"
         findViewById<Button>(R.id.refreshBtn).setOnClickListener { refreshLol() }
     }
 
-    private fun refreshLol() {
-        val textView = findViewById<TextView>(R.id.lolVar)
-        // Instantiate the RequestQueue
+    private fun initServerUrl(user: String) {
         val queue = Volley.newRequestQueue(this)
-        val baseUrl = "https://us-central1-projet3-46f1b.cloudfunctions.net/getServerURL?user=server"
+        val baseUrl = "https://us-central1-projet3-46f1b.cloudfunctions.net/getServerURL?user=$user"
         val stringRequest = StringRequest(
-            Request.Method.GET, baseUrl, { serverUrl ->
+            Request.Method.GET, baseUrl, {
+                serverUrl = "http://$it:10000/lol"
                 println(serverUrl)
-                var url = "http://$serverUrl:10000/lol"
-                val jsonObjectRequest = JsonObjectRequest(
-                    Request.Method.GET, url, null, { lol ->
-                        println(lol)
-                        textView.text = lol.toString()
-                        try {
-                            // TODO Gson https://developer.android.com/training/volley/request-custom.html
-                            textView.text = lol.getInt("lol").toString()
-                        } catch (t: Throwable) {
-                            textView.text = "Malformed response"
-                        }
-                    }, {
-                        textView.text = "That didn't work! Lol error"
-                    })
-                queue.add(jsonObjectRequest)
             }, {
-                textView.text = "That didn't work! Firebase error"
+                serverUrl = it.toString()
             })
         queue.add(stringRequest)
+    }
+
+    private fun setServerUrlWithDialog() {
+        val userDialog: AlertDialog = this.let {
+            val builder = AlertDialog.Builder(it)
+            val input = EditText(this@MainActivity)
+            builder.apply {
+                setTitle("Entrez votre nom de dev")
+                setMessage("Ne rien mettre = server")
+                setPositiveButton("Se connecter") { dialog, _ ->
+                    initServerUrl(if (input.text.isNotEmpty()) input.text.toString() else "server")
+                    dialog.cancel()
+                }
+                setView(input)
+            }
+
+            builder.create()
+        }
+        userDialog.setCancelable(false)
+        userDialog.show()
+    }
+
+    private fun refreshLol() {
+        val textView = findViewById<TextView>(R.id.lolVar)
+        val queue = Volley.newRequestQueue(this)
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, serverUrl, null, {
+                textView.text = it.toString()
+                try {
+                    // TODO Gson https://developer.android.com/training/volley/request-custom.html
+                    textView.text = it.getInt("lol").toString()
+                } catch (t: Throwable) {
+                    textView.text = "Malformed response: $t"
+                }
+            }, {
+                textView.text = "That didn't work! $it"
+            })
+        queue.add(jsonObjectRequest)
     }
 }
