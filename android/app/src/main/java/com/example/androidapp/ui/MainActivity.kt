6e2acb631ac3ba53de61714a10cbe9c1095346ab
Ro se <rosehirigoyen@gmail.com>
2019-09-30 17:20:44 -0400

    Login Activity

--- android/app/src/main/java/com/example/androidapp/ui/MainActivity.kt
+++ android/app/src/main/java/com/example/androidapp/ui/MainActivity.kt
@@ -1,30 +1,19 @@
 package com.example.androidapp.ui
 
 import android.os.Bundle
-import android.view.MenuItem
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import com.crashlytics.android.Crashlytics
-import io.fabric.sdk.android.Fabric
 import com.example.androidapp.R
-import com.google.android.material.navigation.NavigationView
-import kotlinx.coroutines.*
-import org.koin.android.ext.android.inject
+import io.fabric.sdk.android.Fabric
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity(), MainContract.View, CoroutineScope {
-
-    private val controller: MainContract.Controller by inject()
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var toolbar: Toolbar
-    private lateinit var navView: NavigationView
-    private lateinit var textView: TextView
     private lateinit var job: Job
 
     override val coroutineContext: CoroutineContext
@@ -34,54 +23,25 @@ class MainActivity : AppCompatActivity(), MainContract.View, CoroutineScope {
         super.onCreate(savedInstanceState)
         Fabric.with(this, Crashlytics())
         job = Job()
-
         setContentView(R.layout.activity_main)
 
-        toolbar = findViewById(R.id.toolbar)
-        drawerLayout = findViewById(R.id.drawerLayout)
-        navView = findViewById(R.id.navView)
-        textView = findViewById(R.id.lolVar)
-
-        setSupportActionBar(toolbar)
-
-        val actionbar: ActionBar? = supportActionBar
-        actionbar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_menu)
+        findViewById<Button>(R.id.connectionBtn).setOnClickListener{ submitLogin() }
+        findViewById<TextView>(R.id.registerBtn).setOnClickListener{ submitRegister() }
+    }
+    private fun submitLogin() {
+        val username = findViewById<EditText>(R.id.usernameVal).text.toString()
+        val password = findViewById<EditText>(R.id.passwordVal).text.toString()
+        if(username == "rose" && password == "1234") {
+            // val user = "rose;rooose;1234;rose@email.com;10"
+            // val intent = Intent(this@MainActivity, SidePanelActivity::class.java).apply {
+            //     putExtra("user", user)
+            // }
+            // startActivity(intent)
         }
-
-        navView.setNavigationItemSelectedListener { menuItem ->
-            menuItem.isChecked = true
-            drawerLayout.closeDrawers()
-
-            when (menuItem.itemId) {
-                R.id.actionHome -> {
-                    Toast.makeText(this, "Home", Toast.LENGTH_LONG).show()
-                }
-                R.id.actionSettings -> {
-                    Toast.makeText(this, "Settings", Toast.LENGTH_LONG).show()
-                }
-            }
-            true
-        }
-
-        findViewById<TextView>(R.id.lolVar).text = "lol!"
-        findViewById<Button>(R.id.refreshBtn).setOnClickListener { launch {
-            try {
-                textView.text = controller.onRefreshLolAsync()
-            } catch (e: Exception) {
-                textView.text = getString(R.string.errorMessageUnknown)
-            }
-        } }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                drawerLayout.openDrawer(GravityCompat.START)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun submitRegister() {
+        // val intent = Intent(this@MainActivity, RegisterActivity::class.java).apply { }
+        // startActivity(intent)
     }
 }
