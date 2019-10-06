a0d5edb403ff8f2fac765f89c8dfbf831a05cded
Francis Granger <franki_le_pro@hotmail.com>
2019-10-05 22:33:43 -0400

    make it work with ninja

--- server/cmake/third_party.cmake
+++ server/cmake/third_party.cmake
@@ -15,6 +15,7 @@ set(EXTERNAL_LIBS_DIR ${CMAKE_BINARY_DIR}/externals/lib)
 
 ExternalProject_Add(libgflags
     GIT_REPOSITORY https://github.com/gflags/gflags.git
+    BUILD_BYPRODUCTS ${EXTERNAL_LIBS_DIR}/libgflags.a
     CMAKE_ARGS -DBUILD_STATIC_LIBS=on
     CMAKE_ARGS ${COMMON_DEFINES}
     PREFIX libgflags
@@ -22,6 +23,7 @@ ExternalProject_Add(libgflags
 
 ExternalProject_Add(libjson
     GIT_REPOSITORY https://github.com/nlohmann/json.git
+    BUILD_BYPRODUCTS ${EXTERNAL_LIBS_DIR}/libjson.a
     CMAKE_ARGS -DBUILD_TESTING=off
     CMAKE_ARGS ${COMMON_DEFINES}
     PREFIX libjson
@@ -32,6 +34,7 @@ ExternalProject_Add(libgtest
     CMAKE_ARGS -Dgmock_build_tests=off
     CMAKE_ARGS -Dgtest_build_tests=off
     CMAKE_ARGS -Dgtest_build_samples=off
+    BUILD_BYPRODUCTS ${EXTERNAL_LIBS_DIR}/libgtest.a
     CMAKE_ARGS -Dgtest_disable_pthreads=off
     CMAKE_ARGS ${COMMON_DEFINES}
     PREFIX libgtest
@@ -40,6 +43,7 @@ ExternalProject_Add(libgtest
 ExternalProject_Add(libpistache
     GIT_REPOSITORY https://github.com/oktal/pistache.git
     CMAKE_ARGS -DPISTACHE_INSTALL=on
+    BUILD_BYPRODUCTS ${EXTERNAL_LIBS_DIR}/libpistache.a
     CMAKE_ARGS -DPISTACHE_USE_SSL=off
     CMAKE_ARGS ${COMMON_DEFINES}
     PREFIX libpistache
@@ -50,6 +54,7 @@ ExternalProject_Add(libzmq
     CMAKE_ARGS -DBUILD_TESTS=off
     CMAKE_ARGS -DENABLE_DRAFTS=off
     CMAKE_ARGS -DLIBZMQ_PEDANTIC=off
+    BUILD_BYPRODUCTS ${EXTERNAL_LIBS_DIR}/libzmq.a
     CMAKE_ARGS -DWITH_DOCS=off
     CMAKE_ARGS ${COMMON_DEFINES}
     PREFIX libzmq
@@ -57,6 +62,7 @@ ExternalProject_Add(libzmq
 
 ExternalProject_Add(libcppzmq
     GIT_REPOSITORY https://github.com/zeromq/cppzmq.git
+    BUILD_BYPRODUCTS ${EXTERNAL_LIBS_DIR}/libcppzmq.a
     CMAKE_ARGS -DCPPZMQ_BUILD_TESTS=off
     CMAKE_ARGS ${COMMON_DEFINES}
     PREFIX libcppzmq
@@ -64,6 +70,7 @@ ExternalProject_Add(libcppzmq
 add_dependencies(libcppzmq libzmq)
 
 ExternalProject_Add(libcurlpp
+BUILD_BYPRODUCTS ${EXTERNAL_LIBS_DIR}/libcurlpp.a
     GIT_REPOSITORY https://github.com/jpbarrette/curlpp.git
     CMAKE_ARGS ${COMMON_DEFINES}
     PREFIX libcurlpp
