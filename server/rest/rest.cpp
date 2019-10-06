0234da5683baa4cbcbd72a6bf37475ce2e890f9b
Francis Granger <franki_le_pro@hotmail.com>
2019-10-05 23:11:17 -0400

    rest working with gflags

--- server/rest/rest.cpp
+++ server/rest/rest.cpp
@@ -1,20 +1,28 @@
+#include <common/firebase_helper.hpp>
+#include <gflags/gflags.h>
 #include <iostream>
 #include <rest/main_controller.hpp>
-#include <common/firebase_helper.hpp>
 #include <string>
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
+DEFINE_string(user, "server", "Developper using the service");  // NOLINT
+
+int main(int argc, char* argv[]) {
+  for (int i = 0; i < argc; i++) {
+    if (strcmp("--help", argv[i]) == 0) {
+      argv[i] = "--helpshort";
+    }
+  }
+
+  // TODO put in method in common.
+  GFLAGS_NAMESPACE::SetUsageMessage("Rest service");		 // NOLINT
+  GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, false);  // NOLINT
+
   try {
     auto selfIpAddress = FirebaseHelper::getSelfIpAddress();
     std::cout << "Running on: " << selfIpAddress << std::endl;
-    std::future<void> future;
-    if (argc > 1) {
-      future = FirebaseHelper::setIpAddressAsync(selfIpAddress, argv[1]);
-    } else {
-      future = FirebaseHelper::setIpAddressAsync(selfIpAddress);
-    }
+    std::future<void> future = FirebaseHelper::setIpAddressAsync(selfIpAddress, FLAGS_user);
 
     const int kPortNumber = 10000;
     const int kNbThreads = 4;
