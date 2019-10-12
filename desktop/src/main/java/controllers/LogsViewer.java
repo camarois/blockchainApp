6c6157a4ce9632bde392d0c92da9785ce0f78b8b
Bernard <bern97@hotmail.ca>
2019-10-12 16:48:33 -0400

    ca marche mais c'est laitte

--- desktop/src/main/java/controllers/LogsViewer.java
+++ desktop/src/main/java/controllers/LogsViewer.java
@@ -2,31 +2,86 @@ package controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
 public class LogsViewer {
 
   //Nested classes
   class Log {
-    public int number;
-    public String severity;
-    public Date date;
-    public String message;
+
+    public void setNumber(int number) {
+      this.number = number;
+      this.logNum.setText(Integer.toString(number));
+    }
+
+    public void setSeverity(String severity) {
+      this.severity = severity;
+      this.logSev.setText(severity);
+    }
+
+    public void setDate(Date date) {
+      this.date = date;
+      this.logDat.setText(date.toString());
+    }
+
+    public void setMessage(String message) {
+      this.message = message;
+      this.logMes.setText(message);
+    }
+
+    int number;
+    String severity;
+    Date date;
+    String message;
+
+    @FXML
+    HBox haBox;
+    @FXML
+    Label logNum;
+    @FXML
+    Label logSev;
+    @FXML
+    Label logDat;
+    @FXML
+    Label logMes;
+
+    public Log() {
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/LogEntry.fxml"));
+      fxmlLoader.setController(this);
+      try {
+        fxmlLoader.load();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    public HBox getBox() {
+      return haBox;
+    }
+  }
+
+  public class ListViewCell extends ListCell<Log> {
+    @Override
+    public void updateItem(Log log, boolean empty) {
+      super.updateItem(log, empty);
+      if (log != null) {
+        setGraphic(log.getBox());
+      }
+    }
   }
 
   //Attributes
@@ -41,12 +96,17 @@ public class LogsViewer {
   public void initialize() {
     logs = FXCollections.observableArrayList();
     loadLogsFromTxt();
-
     logListView.setItems(logs);
+    logListView.setCellFactory(new Callback<ListView<Log>, ListCell<Log>>() {
+          @Override
+          public ListCell<Log> call(ListView<Log> param) {
+            return new ListViewCell();
+          }
+        }
+    );
   }
 
   private void loadLogsFromTxt() {
-
     try {
       InputStream is = this.getClass().getClassLoader().getResourceAsStream("logs.txt");
       BufferedReader br = new BufferedReader(new InputStreamReader(is));
@@ -54,10 +114,10 @@ public class LogsViewer {
       while ((line = br.readLine()) != null) {
         String[] items = line.split(":");
         Log log = new Log();
-        log.number = Integer.parseInt(items[0].split(" ")[0]);
-        log.severity = items[1].split(" ")[1];
-        log.date = new SimpleDateFormat(" yyyy-MM-dd hh-mm-ss ").parse(items[2]);
-        log.message = items[3].substring(1);
+        log.setNumber(Integer.parseInt(items[0].split(" ")[0]));
+        log.setSeverity(items[1].split(" ")[1]);
+        log.setDate(new SimpleDateFormat(" yyyy-MM-dd hh-mm-ss ").parse(items[2]));
+        log.setMessage(items[3].substring(1));
         logs.add(log);
       }
     } catch (IOException | ParseException e) {
