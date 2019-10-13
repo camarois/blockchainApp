f346be8e6e9bb94bb7d4415e9b8762dfc49881be
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-10-13 12:23:30 -0400

    server/tests: test blockchain loading

---
+++ server/tests/blockchain_test.cpp
@@ -0,0 +1,48 @@
+#include <cstdlib>
+#include <exception>
+#include <filesystem>
+#include <gtest/gtest.h>
+#include <miner/blockchain.hpp>
+
+#include "utils.hpp"
+
+TEST(BlockChainTest, loadFromEmptyDir) {
+  std::filesystem::path blockDir = Tests::createEmptyDir("blockchain/load-from-empty-dir");
+  Miner::BlockChain loaded(blockDir);
+
+  ASSERT_EQ(loaded.difficulty(), 3);
+  ASSERT_EQ(loaded.lastBlock()->getID(), 0);
+}
+
+TEST(BlockChainTest, loadWithoutMetadata) {
+  std::filesystem::path blockDir = Tests::getDir("blockchain/load-without-metadata");
+  Miner::BlockChain loaded(blockDir);
+
+  ASSERT_EQ(loaded.difficulty(), 3);
+  ASSERT_EQ(loaded.lastBlock()->getID(), 0);
+}
+
+TEST(BlockChainTest, loadWithMissingBlock) {
+  std::filesystem::path blockDir = Tests::getDir("blockchain/load-with-missing-block");
+  
+  ASSERT_THROW(Miner::BlockChain loaded(blockDir), std::runtime_error);
+}
+
+TEST(BlockChainTest, loadWithMismatchID) {
+  std::filesystem::path blockDir = Tests::getDir("blockchain/load-with-mismatch-id");
+  
+  ASSERT_THROW(Miner::BlockChain loaded(blockDir), std::runtime_error);
+}
+
+TEST(BlockChainTest, loadFromDir) {
+  std::filesystem::path blockDir = Tests::getDir("blockchain/load-from-file");
+  Miner::BlockChain loaded(blockDir);
+
+  ASSERT_EQ(loaded.difficulty(), 10);
+  ASSERT_EQ(loaded.lastBlock()->getID(), 420);
+  ASSERT_EQ(loaded.lastBlock()->getNonce(), 0);
+  ASSERT_EQ(loaded.lastBlock()->getPreviousHash(), "eed82d9682fb1a4a37ecceba76052738f687f3c5e5fd317c940471e8413d140f");
+  ASSERT_EQ(loaded.lastBlock()->getData()[0], "Kathy Brousseau");
+  ASSERT_EQ(loaded.lastBlock()->getData()[1], "Aya Bordeleau");
+  ASSERT_EQ(loaded.lastBlock()->getData()[2], "Jeffrey Brisebois");
+}
