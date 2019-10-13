fb5344d1dde065501b254513cc6657020a8e5992
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-10-13 12:23:06 -0400

    server/miner: write most of the blockchain loading

--- server/miner/include/miner/blockchain.hpp
+++ server/miner/include/miner/blockchain.hpp
@@ -1,6 +1,7 @@
 #ifndef MINER_BLOCKCHAIN_HPP
 #define MINER_BLOCKCHAIN_HPP
 
+#include <exception>
 #include <filesystem>
 #include <list>
 #include <memory>
@@ -9,19 +10,52 @@
 
 namespace Miner {
 
+class BlockChain;
+
 using BlockPtr = std::shared_ptr<Block>;
+using BlockChainUPtr = std::unique_ptr<BlockChain>;
 
 class BlockChain {
  public:
-  explicit BlockChain();
+  BlockChain();
   explicit BlockChain(std::filesystem::path blockDir);
 
+  unsigned int difficulty() const;
   BlockPtr newBlock();
   BlockPtr lastBlock() const;
+
+  // NOLINTNEXTLINE(readability-identifier-naming, google-runtime-references)
+  friend void to_json(nlohmann::json& j, const BlockChain& obj);
+
+  // NOLINTNEXTLINE(readability-identifier-naming, google-runtime-references)
+  friend void from_json(const nlohmann::json& j, BlockChain& obj);
+
  private:
   unsigned int difficulty_;
-  std::list<BlockPtr> blocks_;
+  std::map<unsigned int, BlockPtr> blocks_;
+
+  const std::string kDifficulty_ = "difficulty";
+  const std::string kLastBlock_ = "last_block";
+  const std::string kMetadata_ = "metadata";
+
+  static BlockChainUPtr loadFromJSON(std::filesystem::path path);
 };
+
+// NOLINTNEXTLINE(readability-identifier-naming, google-runtime-references)
+inline void to_json(nlohmann::json& j, const BlockChain& obj) {
+  j = {
+    {obj.kLastBlock_, obj.lastBlock()->getID()},
+    {obj.kDifficulty_, obj.difficulty()},
+  };
+}
+
+// NOLINTNEXTLINE(readability-identifier-naming, google-runtime-references)
+inline void from_json(const nlohmann::json& j, BlockChain& obj) {
+  unsigned int lastBlock;
+  j[obj.kLastBlock_].get_to(lastBlock);
+  j[obj.kDifficulty_].get_to(obj.difficulty_);
+  obj.blocks_.insert(std::pair<unsigned int, BlockPtr>(lastBlock, nullptr));
+}
 
 } // namespace Miner
 
--- server/miner/src/blockchain.cpp
+++ server/miner/src/blockchain.cpp
@@ -1,28 +1,88 @@
+#include <fstream>
+
 #include "miner/blockchain.hpp"
 
 namespace Miner {
 
-BlockChain::BlockChain() { difficulty_ = 3; }
+BlockChain::BlockChain() {
+  difficulty_ = 3;
+  newBlock();
+}
 
-BlockChain::BlockChain(std::filesystem::path blockDir) : BlockChain() {
+BlockChain::BlockChain(std::filesystem::path blockDir) {
   if (blockDir.empty()) {
+    difficulty_ = 3;
+    newBlock();
     return;
   }
 
-  int lastBlock;
-  for (lastBlock = 0; std::filesystem::exists(blockDir); lastBlock++) {
-    blockDir.remove_filename();
-    blockDir.append(std::to_string(lastBlock));
+  std::filesystem::path metadataPath(blockDir/kMetadata_);
+  if (!std::filesystem::exists(metadataPath)) {
+    difficulty_ = 3;
+    newBlock();
+    return;
   }
 
-  if (lastBlock == 0) {
-    // TODO(gabriel): no blockchain files
+  BlockChainUPtr metadata = loadFromJSON(metadataPath);
+  difficulty_ = metadata->difficulty_;
+  unsigned int lastBlockID = metadata->blocks_.rbegin()->first;
+
+  std::filesystem::path blockPath(blockDir/std::to_string(lastBlockID));
+
+  BlockPtr lastBlock;
+  try {
+    lastBlock = std::make_shared<Block>(blockPath);
+  } catch(const std::invalid_argument &e) {
+    throw std::runtime_error("block #" + std::to_string(lastBlockID) + " doesn't exist");
   }
-  lastBlock--;
+
+  if (lastBlock->getID() != lastBlockID) {
+    throw std::runtime_error("mismatch ID in block #" + std::to_string(lastBlockID));
+  }
+
+  blocks_.insert(std::pair<unsigned int, BlockPtr>(lastBlock->getID(), lastBlock));
 }
 
-BlockPtr BlockChain::newBlock() { return std::make_shared<Block>(); }
+unsigned int BlockChain::difficulty() const { return difficulty_; }
 
-BlockPtr BlockChain::lastBlock() const { return std::make_shared<Block>(); }
+BlockPtr BlockChain::newBlock() {
+  unsigned int nextID;
+  std::string previousHash;
+
+  BlockPtr last = lastBlock();
+  if (last == nullptr) {
+    nextID = 0;
+    previousHash = "";
+  } else {
+    nextID = last->getID() + 1;
+    previousHash = last->getHash();
+  }
+
+  BlockPtr block = std::make_shared<Block>(nextID, previousHash);
+  blocks_.insert(std::pair<unsigned int, BlockPtr>(block->getID(), block));
+
+  return block;
+}
+
+BlockPtr BlockChain::lastBlock() const {
+  if (blocks_.size() == 0) {
+    return nullptr;
+  }
+
+  return blocks_.rbegin()->second;
+}
+
+BlockChainUPtr BlockChain::loadFromJSON(std::filesystem::path metadataPath) {
+  std::ifstream metadataFile(metadataPath, std::ifstream::in);
+  if (metadataFile.fail()) {
+    return nullptr;
+  }
+  
+  nlohmann::json json;
+  metadataFile >> json;
+  metadataFile.close();
+
+  return std::make_unique<BlockChain>(json.get<BlockChain>());
+}
 
 }  // namespace Miner
