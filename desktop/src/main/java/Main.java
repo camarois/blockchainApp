0a0f7ae0d8f871cc3a1bcc060b450454d2616f04
Bernard <bern97@hotmail.ca>
2019-10-15 17:45:10 -0400

    fix

--- desktop/src/main/java/Main.java
+++ desktop/src/main/java/Main.java
@@ -1,11 +1,10 @@
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import services.RestService;
 
 import java.util.Objects;
 
