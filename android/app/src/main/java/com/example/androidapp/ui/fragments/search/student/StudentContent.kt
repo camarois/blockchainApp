1243ee5c75a22eaa9ac7763fb507fcc66915b1fd
Ro se <rosehirigoyen@gmail.com>
2019-10-23 19:46:45 -0400

    Changed student format to fit with API

--- android/app/src/main/java/com/example/androidapp/ui/fragments/search/student/StudentContent.kt
+++ android/app/src/main/java/com/example/androidapp/ui/fragments/search/student/StudentContent.kt
@@ -21,8 +21,7 @@ object StudentContent {
     private val item_map: MutableMap<String, StudentItem> = HashMap()
 
     private const val COUNT = 25
-    private const val MAX_GPA = 4.0
-    private const val MIN_GRAD_YEAR = 2000
+    private const val BASE_STUDENT_CODE = 1800000
 
     init {
         // Add some sample items.
@@ -38,6 +37,6 @@ object StudentContent {
 
     private fun createStudentItem(position: Int): StudentItem {
         return StudentItem(position.toString(),
-            "Prenom Nom $position", "GENIE INFO", (position * MAX_GPA / COUNT).toFloat(), MIN_GRAD_YEAR + position, "notes.pdf")
+            "Prenom Nom $position", BASE_STUDENT_CODE + position, "notes.pdf")
     }
 }
--- android/app/src/main/java/com/example/androidapp/ui/fragments/search/student/StudentItem.kt
+++ android/app/src/main/java/com/example/androidapp/ui/fragments/search/student/StudentItem.kt
@@ -3,6 +3,6 @@ package com.example.androidapp.ui.fragments.search.student
 /**
  * A student item representing a piece of content.
  */
-data class StudentItem(val id: String, val name: String, val degree: String, val gpa: Float, val grad: Int, val transcript: String) {
-    override fun toString(): String = "Nom: $name, Programme: $degree, Cote: $gpa"
+data class StudentItem(val id: String, val name: String, val code: Int, val transcript: String) {
+    override fun toString(): String = "Nom: $name, Matricule: $code"
 }
