616dff70a7f6ca2436724594cc115501a12b5dca
Francis Granger <franki_le_pro@hotmail.com>
2019-10-26 15:24:47 -0400

    modif models to not break

--- server/rest/src/transaction_controller.cpp
+++ server/rest/src/transaction_controller.cpp
@@ -1,11 +1,10 @@
+#include <common/base64.hpp>
 #include <common/models.hpp>
 #include <rest/transaction_controller.hpp>
 
 namespace Rest {
 
-TransactionController::TransactionController(const std::shared_ptr<Rest::CustomRouter>& router) {
-  setupRoutes(router);
-}
+TransactionController::TransactionController(const std::shared_ptr<Rest::CustomRouter>& router) { setupRoutes(router); }
 
 void TransactionController::setupRoutes(const std::shared_ptr<Rest::CustomRouter>& router) {
   router->post(kBasePath_, Pistache::Rest::Routes::bind(&TransactionController::handleTransaction, this));
@@ -14,6 +13,9 @@ void TransactionController::setupRoutes(const std::shared_ptr<Rest::CustomRouter
 void TransactionController::handleTransaction(const Pistache::Rest::Request& request,
                                               Pistache::Http::ResponseWriter response) {
   Common::Models::TransactionRequest transactionRequest = nlohmann::json::parse(request.body());
+  // std::cout << transactionRequest.base64Pdf << std::endl;
+  // std::cout << Common::Base64::decode(transactionRequest.base64Pdf) << std::endl;
+  // response.send(Pistache::Http::Code::I_m_a_teapot, Common::Base64::decode(transactionRequest.base64Pdf));
   response.send(Pistache::Http::Code::I_m_a_teapot, "TODO");
 }
 
