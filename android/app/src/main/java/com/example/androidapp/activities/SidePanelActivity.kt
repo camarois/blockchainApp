ce458d52f6c542557e4cd5efef1e2fbd1f0317a4
camarois <charles.marois.quebec@gmail.com>
2019-10-29 11:41:59 -0400

    test

--- android/app/src/main/java/com/example/androidapp/activities/SidePanelActivity.kt
+++ android/app/src/main/java/com/example/androidapp/activities/SidePanelActivity.kt
@@ -51,7 +51,7 @@ class SidePanelActivity : AppCompatActivity(), CoroutineScope, SearchFragment.On
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_search, R.id.nav_register
+                R.id.nav_home, R.id.nav_search, R.id.nav_settings, R.id.nav_register
             ), sidePanelDrawerLayout
         )
 
