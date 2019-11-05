3853b629c0b72d7829d3723fab240e62bdabc371
Charles-Auguste Marois <charles.marois.quebec@gmail.com>
2019-11-05 18:44:55 -0500

    Student results fragment

--- android/app/src/main/java/com/example/androidapp/Models.kt
+++ android/app/src/main/java/com/example/androidapp/Models.kt
@@ -20,6 +20,16 @@ data class CourseResponse(
     @SerializedName("resultats") val oldPassword: List<StudentItem>
 )
 
+data class StudentResults(
+    @SerializedName("courses") val courses: List<CourseResult>
+)
+
+data class CourseResult(
+    @SerializedName("sigle") val acronym: String,
+    @SerializedName("trimestre") val trimester: String,
+    @SerializedName("note") val grade: String
+)
+
 data class CourseItem(val code: String, val name: String, val trimester: String, val students: List<StudentItem>) {
     override fun toString(): String = "Nom: $name, Sigle: $code, Name "
 }
--- android/app/src/main/java/com/example/androidapp/fragments/searchStudent/DetailedStudentFragment.kt
+++ android/app/src/main/java/com/example/androidapp/fragments/searchStudent/DetailedStudentFragment.kt
@@ -5,32 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidapp.CourseResult
 import com.example.androidapp.R
 import com.example.androidapp.fragments.searchStudent.student.StudentItem
 
-import com.example.androidapp.services.RestRequestService
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import org.koin.android.ext.android.get
-import kotlin.coroutines.CoroutineContext
+import kotlinx.android.synthetic.main.fragment_detailed_student.view.*
 
 class DetailedStudentFragment(
-    private val student: StudentItem
-) : Fragment(), CoroutineScope {
+    private val student: StudentItem,
+    private val results: List<CourseResult>
+) : Fragment() {
 
-    private var restService: RestRequestService = get()
-    private lateinit var job: Job
+    private var columnCount = 1
 
-    override val coroutineContext: CoroutineContext
-        get() = job + Dispatchers.Main
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val viewCreated = view.results_list
+        viewCreated.adapter =
+            StudentResultsRecyclerViewAdapter(
+                results
+            )
+        super.onViewCreated(view, savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        job = Job()
-        return inflater.inflate(R.layout.fragment_detailed_student, container, false)
+        val view = inflater.inflate(R.layout.fragment_detailed_student, container, false)
+        view.student_code.text = student.code.toString()
+        view.student_name.text = student.name
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter =
+                    StudentResultsRecyclerViewAdapter(
+                        results
+                    )
+            }
+        }
+        return view
     }
 }
