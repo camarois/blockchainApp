d713c21516e87ec38323131dc22a87380c1cf3ad
Francis Granger <franki_le_pro@hotmail.com>
2019-11-11 17:38:03 -0500

    make magic enum work in logger

--- server/common/src/logger.cpp
+++ server/common/src/logger.cpp
@@ -2,11 +2,10 @@
 
 #include <common/database.hpp>
 #include <common/format_helper.hpp>
+#include <magic_enum.hpp>
 
 namespace Common {
 
-std::vector<std::string> Logger::severities = {"ERREUR", "ATTENTION", "INFO"};
-bool Logger::isInitialized = false;
 std::shared_ptr<Logger> Logger::instance;
 
 // NOLINTNEXTLINE
@@ -17,33 +16,28 @@ Logger::Logger(int logSessionId, const std::string& dbPath) : logSessionId_(logS
 std::shared_ptr<Logger> Logger::get() { return instance; }
 
 void Logger::init(const std::string& dbPath) {
-  if (!isInitialized) {
+  if (!Logger::instance) {
     Common::Database db(dbPath);
     auto logSessionId = db.addLogSession();
     instance = std::make_shared<Logger>(logSessionId, dbPath);
-    isInitialized = true;
     std::cout << "Logger created" << std::endl;
   }
 }
 
-void Logger::error(int provenance, const std::string& message) {
-  log(static_cast<int>(Severity::ERROR), provenance, message, std::cerr);
-}
+void Logger::error(int provenance, const std::string& message) { log(Severity::ERROR, provenance, message, std::cerr); }
 
 void Logger::attention(int provenance, const std::string& message) {
-  log(static_cast<int>(Severity::ATTENTION), provenance, message, std::cout);
+  log(Severity::ATTENTION, provenance, message, std::cout);
 }
 
-void Logger::info(int provenance, const std::string& message) {
-  log(static_cast<int>(Severity::INFO), provenance, message, std::cout);
-}
+void Logger::info(int provenance, const std::string& message) { log(Severity::INFO, provenance, message, std::cout); }
 
-void Logger::log(int severity, int provenance, const std::string& message, std::ostream& stream) {
+void Logger::log(Severity severity, int provenance, const std::string& message, std::ostream& stream) {
   std::lock_guard<std::mutex> lock(mutex_);
   Common::Database db(dbPath_);
   auto nowStr = Common::FormatHelper::nowStr();
   stream << std::endl
-         << logCount_ << ": " << severities.at(severity) << ": " << nowStr << ": " << provenance << ": " << message
+         << logCount_ << ": " << magic_enum::enum_name(severity) << ": " << nowStr << ": " << provenance << ": " << message
          << std::endl;
   db.addLog(logCount_, severity, provenance, nowStr, message, logSessionId_);
   ++logCount_;
