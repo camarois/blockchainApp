1575fdc67015436fcf8d3bfca800e76539fcc392
Francis Granger <franki_le_pro@hotmail.com>
2019-11-12 12:31:09 -0500

    fix build

--- server/build.sh
+++ server/build.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 BUILD_DIRECTORY="$SCRIPT_DIR/build"
 mkdir -p "$BUILD_DIRECTORY"
 pushd "$BUILD_DIRECTORY" || exit 1
-cmake -GNinja -DCMAKE_BUILD_TYPE=Debug .. # Debug RelWithDebInfo Release
+cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ .. # Debug RelWithDebInfo Release
 if [[ -z ${1+x} ]]; then
 	ninja -j 4
 else
