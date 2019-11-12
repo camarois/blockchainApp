b0fae65989702abe061e5f732c4035f439a1020a
Francis Granger <franki_le_pro@hotmail.com>
2019-11-12 13:06:39 -0500

    also add the build .sh modif

--- server/build.sh
+++ server/build.sh
@@ -11,8 +11,8 @@ mkdir -p "$BUILD_DIRECTORY"
 pushd "$BUILD_DIRECTORY" || exit 1
 cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ .. # Debug RelWithDebInfo Release
 if [[ -z ${1+x} ]]; then
-	ninja -j 4
+	ninja -j 4 || exit 1
 else
-	ninja -j "$1"
+	ninja -j "$1" || exit 1
 fi
 popd || exit 1
