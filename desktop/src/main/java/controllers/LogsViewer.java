83f6403f09ba7e00dee6681800a9e0d7e4ad77ca
camarois <charles.marois.quebec@gmail.com>
2019-11-22 13:18:43 -0500

    fix logs view.

--- desktop/src/main/java/controllers/LogsViewer.java
+++ desktop/src/main/java/controllers/LogsViewer.java
@@ -2,8 +2,11 @@ package controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import models.AllUsersRequest;
 import models.LogsRequest;
 import models.LogsResponse;
 import services.RestService;
@@ -11,37 +14,40 @@ import services.RestService;
 import java.util.*;
 
 public class LogsViewer {
-    private int provenanceIndex = 0;
     private List<String> provenance = List.of("serveurweb", "1", "2", "3");
     private ObservableList<LogsResponse.Log> logsList;
-    @FXML private TableView<LogsResponse.Log> logTableView;
+    @FXML
+    public VBox vboxLogs;
+    @FXML
+    private TableView<LogsResponse.Log> logTableView;
 
-    @FXML public void initialize() {
+    @FXML
+    public void initialize() {
         logsList = FXCollections.observableArrayList();
         logTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         logTableView.setItems(logsList);
-        startTask();
+        updateTbl();
     }
 
-    private void startTask() {
-        new Timer().scheduleAtFixedRate(new TimerTask() {
-            public void run() {
-                try {
-                    String origin = provenance.get(provenanceIndex);
-                    LogsRequest request =  logsList.filtered(log ->
-                            log.getProvenance().equals((origin))).isEmpty() ? new LogsRequest(0) :
-                            new LogsRequest(logsList.stream().filter(log ->
-                                    log.getProvenance().equals(origin)
-                            ).mapToInt(LogsResponse.Log::getNumber).max().getAsInt());
-                    LogsResponse logsResponse = RestService.postRequest(RestService.urls.getLogs() + origin,
-                            request, LogsResponse.class);
-                    logsResponse.logs.forEach((log) -> log.setProvenance(origin));
-                    logsList.addAll(logsResponse.logs);
-                    provenanceIndex  = provenanceIndex == 3 ? 0 : ++provenanceIndex;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+    public void updateTbl() {
+        try {
+            for (String origin : provenance) {
+                LogsRequest request = logsList.filtered(log ->
+                        log.getProvenance().equals((origin))).isEmpty() ? new LogsRequest(0) :
+                        new LogsRequest(logsList.stream().filter(log ->
+                                log.getProvenance().equals(origin)
+                        ).mapToInt(LogsResponse.Log::getNumber).max().getAsInt());
+                LogsResponse logsResponse = RestService.postRequest(RestService.urls.getLogs() + origin,
+                        request, LogsResponse.class);
+                logsResponse.logs.forEach((log) -> log.setProvenance(origin));
+                logsList.addAll(logsResponse.logs);
             }
-        }, 250, 10000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onClickRefresh(ActionEvent actionEvent) {
+        updateTbl();
     }
 }
