df99e1a0bab5a4829d4da06197af84aa954e8136
Bernard Meunier <bern97@hotmail.ca>
2019-11-27 15:09:15 -0500

    toto

--- desktop/src/main/java/controllers/ChainViewerController.java
+++ desktop/src/main/java/controllers/ChainViewerController.java
@@ -1,7 +1,10 @@
 package controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -16,9 +19,29 @@ public class ChainViewerController {
     public TableColumn hashCol;
     @FXML
     public TableColumn contentCol;
+    @FXML
+    public ComboBox<String> minerChooser;
 
+    @FXML
+    public void initialize() {
+        fillCombobox();
+    }
 
     public void onClickRefresh(ActionEvent actionEvent) {
     }
 
+    public void fillCombobox() {
+
+        ObservableList<String> miners = FXCollections.observableArrayList();
+        for (int i = 1; i <= 3; i++) {
+            miners.add("Mineur " + i);
+        }
+
+        minerChooser.getItems().addAll(miners);
+    }
+
+    public void onComboBoxSelect(ActionEvent actionEvent) {
+        String value = minerChooser.getValue();
+        System.out.println(value.charAt(value.length() - 1));
+    }
 }
