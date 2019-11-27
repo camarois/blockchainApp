7f3b0d29df02255ce75fcc349ca78afb5e703542
camarois <charles.marois.quebec@gmail.com>
2019-11-27 16:11:41 -0500

    start tests.

--- server/tests/database_test.cpp
+++ server/tests/database_test.cpp
@@ -4,14 +4,14 @@
 
 TEST(Sqlite3Tests, get_user) {
   Common::Database db("test-blockchain.db");
-  Common::Models::LoginRequest expectedUser = {"Anne-Sophie Provencher", "LOL1234!"};
+  Common::Models::LoginRequest expectedlogin = {"Anne-Sophie Provencher", "LOL1234!"};
   std::string expectedHash = "da6a850377faa387cea7c58a6ebd5935d5502a95aa0993848f8ae4ab8efc68ad";
 
-  db.addUser(expectedUser);
+  db.addUser({expectedlogin, false, false});
 
-  auto salt = db.getSalt(expectedUser.username);
+  auto salt = db.getSalt({expectedlogin.username});
   ASSERT_TRUE(salt.has_value());
-  auto receivedUser = db.containsUser(expectedUser, salt.value());
+  auto receivedUser = db.containsUser({expectedlogin, salt.value()});
   ASSERT_TRUE(receivedUser);
 }
 
@@ -24,16 +24,16 @@ TEST(Sqlite3Tests, test_transaction) {
   expectedResults.push_back(expectedResult2);
 
   Common::Models::TransactionRequest transaction = {"inf3995", "Projet3", 20193, expectedResults};
-  std::optional<int> classId = db.checkForExistingClass(transaction.acronym, transaction.trimester);
+  std::optional<int> classId = db.checkForExistingClass({transaction.acronym, transaction.trimester});
   if (classId){
     db.deleteExistingClass(classId.value());
     db.deleteExistingResults(classId.value());
   }
   int newClassId = db.addNewClass(transaction);
-  db.addNewResult(transaction, newClassId);
+  db.addNewResult({transaction, newClassId});
 
   Common::Models::ClassesRequest classesRequest = {"inf3995", 20193};
-  classId = db.checkForExistingClass(classesRequest.acronym, classesRequest.trimester);
+  classId = db.checkForExistingClass({classesRequest.acronym, classesRequest.trimester});
   std::vector<Common::Models::Result> receivedResults;
   if (classId){
     receivedResults = db.getClassResult(classId.value());
