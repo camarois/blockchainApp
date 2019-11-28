ab4d2293cf94a572554d5dd6da34eef12afa920a
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-11-27 23:42:05 -0500

    server: reset systemd logs

--- server/package/inf3995-reset.sh
+++ server/package/inf3995-reset.sh
@@ -6,5 +6,8 @@ systemctl stop inf3995-rest
 systemctl disable inf3995-miner
 systemctl disable inf3995-rest
 
+journalctl --rotate
+journalctl --vacuum-time=1s
+
 rm -rf /srv/inf3995/miner/*
 rm -rf /srv/inf3995/rest/*
